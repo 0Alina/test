5ba97d07a39c0d7a3432289d2bae101068e9a4c6
0Alina <alina.glodea@gmail.com>
2024-06-21 17:41:20 +0300

    add

--- src/oop/Student.java
+++ src/oop/Student.java
@@ -4,6 +4,7 @@ public class Student extends Specialitate {
     private String NP;
 
     public Student(String nume, String titlu, String NP) {
-
+        super(nume, titlu);
+        this.NP = NP;
     }
 }
